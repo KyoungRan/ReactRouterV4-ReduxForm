a006bc079a7e3e6b5009f8bf80defffb20a4c293
KyoungRan <kyoungrankang@gmail.com>
2017-05-16 00:56:04 +0900

    144. Receiving New Posts

--- ReduxSimpleStarter/src/reducers/reducer_posts.js
+++ ReduxSimpleStarter/src/reducers/reducer_posts.js
@@ -1,8 +1,15 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST } from '../actions';
 import _ from 'lodash';
 
 export default function(state = {}, action) {
     switch (action.type) {
+        case FETCH_POST:
+            // const post = action.payload.data;
+            // const newState = { ...state };
+            // newState[post.id] = post;
+            // return newState;
+            // same here~
+            return { ...state, [action.payload.data.id]: action.payload.data };
         case FETCH_POSTS:
             // console.log(action.paylosd.data);   // [ post1, post2 ]
             // { 4: post }
