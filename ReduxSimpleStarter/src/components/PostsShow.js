0f9d530b6b76839d5138f05e8afd686e13adf88b
KyoungRan <kyoungrankang@gmail.com>
2017-05-16 01:11:56 +0900

    146. Data Dependencies

--- ReduxSimpleStarter/src/components/PostsShow.js
+++ ReduxSimpleStarter/src/components/PostsShow.js
@@ -4,15 +4,23 @@ import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
     componentDidMount() {
-        const { id } = this.props.match.params.id;
-        this.props.fetchPost();
+        const { id } = this.props.match.params;
+        this.props.fetchPost(id);
     }
 
     render() {
         // posts[this.props.match.params.id]; // the post we want to show
+        const { post } = this.props;
+
+        if (!post) {
+            return <div>Loading...</div>;
+        }
+
         return (
             <div>
-                Posts Show
+                <h3>{post.title}</h3>
+                <h6>Categories: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         );
     }
